045d3c81ba07b87f12876678322816ba884488d7
IFNMG <IFNMG@192.168.165.14>
2019-11-20 22:42:12 -0200

    Criando class Motor gets sets contrutores etc

---
+++ src/Motor.java
@@ -0,0 +1,25 @@
+
+public class Motor {
+    private double potencia;
+    private double rpm;
+
+    
+    public double getPotencia() {
+        return potencia;
+    }
+
+    
+    public void setPotencia(double potencia) {
+        this.potencia = potencia;
+    }
+
+    
+    public double getRpm() {
+        return rpm;
+    }
+
+    
+    public void setRpm(double rpm) {
+        this.rpm = rpm;
+    }
+}
