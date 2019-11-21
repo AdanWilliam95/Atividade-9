27b011558993529f004e16f6affe1cafd95de2bb
IFNMG <IFNMG@192.168.165.14>
2019-11-20 22:39:37 -0200

    Gets e Sets

---
+++ src/Produto.java
@@ -0,0 +1,49 @@
+
+public class Produto {
+ private String cod;
+ private String nome;
+ private String descricao;
+ private double preco; 
+
+   
+    public String getCod() {
+        return cod;
+    }
+
+ 
+    public void setCod(String cod) {
+        this.cod = cod;
+    }
+
+    
+    public String getNome() {
+        return nome;
+    }
+
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    
+    public String getDescricao() {
+        return descricao;
+    }
+
+   
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+   
+    public double getPreco() {
+        return preco;
+    }
+
+   
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+    
+}
+
